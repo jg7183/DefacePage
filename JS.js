7c8771662509b162b0c6b5cc0085f609c9755d86
jg7183 <jg7183@yahoo.com.au>
2014-05-18 08:52:57 +1000

    First Commit
    Everything seems to be working fine.

---
+++ JS.js
@@ -0,0 +1,5 @@
+function nClick(){
+	document.getElementById("clickMe").style.left = Math.floor((Math.random() * 900) + 1)+"px";
+	document.getElementById("clickMe").style.top = Math.floor((Math.random() * 450) + 1)+"px";
+	
+	}
